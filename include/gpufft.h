9401da9b3542922195917bd3db53ac355ff56062
Bryce Allen <bdallen@uchicago.edu>
2020-10-23 15:46:44 +0000

    add gpufft lib

---
+++ include/gpufft.h
@@ -0,0 +1,70 @@
+#include "gtensor/gtensor.h"
+
+#ifdef GTENSOR_DEVICE_CUDA
+
+#include <cufft.h>
+typedef cufftHandle gpufft_handle_t;
+typedef cudaStream_t gpufft_stream_t;
+
+typedef cufftType gpufft_transform_t;
+#define GPUFFT_Z2D CUFFT_Z2D
+#define GPUFFT_D2Z CUFFT_D2Z
+#define GPUFFT_C2R CUFFT_C2R
+#define GPUFFT_R2C CUFFT_R2C
+
+typedef cufftDoubleReal gpufft_double_real_t;
+typedef cufftReal gpufft_real_t;
+typedef cufftDoubleComplex gpufft_double_complex_t;
+typedef cufftComplex gpufft_complex_t;
+
+#elif defined(GTENSOR_DEVICE_HIP)
+
+#include "hipfft.h"
+typedef hipfftHandle gpufft_handle_t;
+typedef hipStream_t gpufft_stream_t;
+
+typedef hipfftType gpufft_transform_t;
+#define GPUFFT_Z2D HIPFFT_Z2D
+#define GPUFFT_D2Z HIPFFT_D2Z
+#define GPUFFT_C2R HIPFFT_C2R
+#define GPUFFT_R2C HIPFFT_R2C
+
+typedef hipfftDoubleReal gpufft_double_real_t;
+typedef hipfftReal gpufft_real_t;
+typedef hipfftDoubleComplex gpufft_double_complex_t;
+typedef hipfftComplex gpufft_complex_t;
+
+#elif defined(GTENSOR_DEVICE_SYCL)
+
+#include "CL/sycl.hpp"
+#include "oneapi/mkl.hpp"
+typedef cl::sycl::queue* gpublas_handle_t;
+typedef cl::sycl::queue* gpublas_stream_t;
+#endif
+
+//#ifdef __cplusplus
+// extern "C" {
+//#endif
+//
+void gpufft_plan_many(gpufft_handle_t* handle, int rank, int* n, int istride,
+                      int idist, int ostride, int odist,
+                      gpufft_transform_t type, int batchSize);
+
+void gpufft_plan_destory(gpufft_handle_t handle);
+/*
+  auto result = cufftPlanMany(&plan_c2r_, 1, &n_, nullptr, 1, nk_, nullptr, 1,
+                              n, CUFFT_Z2D, howmany_);
+  assert(result == CUFFT_SUCCESS);
+  result = cufftPlanMany(&plan_r2c_, 1, &n_, nullptr, 1, n_, nullptr, 1, nk_,
+                         CUFFT_D2Z, howmany_);
+                         */
+
+void gpufft_exec_z2d(gpufft_handle_t handle, gpufft_double_complex_t* indata,
+                     gpufft_double_real_t* outdata);
+
+void gpufft_exec_d2z(gpufft_handle_t handle, gpufft_double_real_t* indata,
+                     gpufft_double_complex_t* outdata);
+
+//#ifdef __cplusplus
+//}
+//#endif
---
+++ src/gpufft.cxx
@@ -0,0 +1,74 @@
+#include <gtensor/gtensor.h>
+
+#include "gpufft.h"
+
+void gpufft_plan_many(gpufft_handle_t* handle, int rank, int* n, int istride,
+                      int idist, int ostride, int odist,
+                      gpufft_transform_t type, int batchSize)
+{
+#ifdef GTENSOR_DEVICE_CUDA
+  auto result = cufftPlanMany(handle, rank, n, nullptr, istride, idist, nullptr,
+                              ostride, odist, type, batchSize);
+  assert(result == CUFFT_SUCCESS);
+#elif defined(GTENSOR_DEVICE_HIP)
+  auto result = hipfftPlanMany(handle, rank, n, nullptr, istride, idist,
+                               nullptr, ostride, odist, type, batchSize);
+  assert(result == HIPFFT_SUCCESS);
+  /*
+  rocfft_plan_description desc = NULL;
+  rocfft_plan_description_create(&desc);
+  rocfft_plan_description_set_data_layout(
+        desc,
+        // input data format:
+        rocfft_array_type_real,
+        // output data format:
+        rocfft_array_type_real,
+        nullptr, // in offsets
+        nullptr, // out offsets
+        rank, // input stride length
+        istride, // input stride data
+        idist, // input batch distance
+        rank, // output stride length
+        ostride, // output stride data
+        odist); // ouptut batch distance
+  auto result = rocfft_plan_create(handle, rocfft_placment_inline, type,
+                                   rocfft_precision_double, rank, n, batchSize,
+  desc); assert(result == rocfft_success);
+  */
+#endif
+}
+
+void gpufft_plan_destory(gpufft_handle_t handle)
+{
+#ifdef GTENSOR_DEVICE_CUDA
+  auto result = cufftDestroy(handle);
+  assert(result == CUFFT_SUCCESS);
+#elif defined(GTENSOR_DEVICE_HIP)
+  auto result = hipfftDestroy(handle);
+  assert(result == rocfft_success);
+#endif
+}
+
+void gpufft_exec_z2d(gpufft_handle_t handle, gpufft_double_complex_t* indata,
+                     gpufft_double_real_t* outdata)
+{
+#ifdef GTENSOR_DEVICE_CUDA
+  auto result = cufftExecZ2D(handle, indata, outdata);
+  assert(result == CUFFT_SUCCESS);
+#elif defined(GTENSOR_DEVICE_HIP)
+  auto result = hipfftExecZ2D(handle, indata, outdata);
+  assert(result == rocfft_success);
+#endif
+}
+
+void gpufft_exec_d2z(gpufft_handle_t handle, gpufft_double_real_t* indata,
+                     gpufft_double_complex_t* outdata)
+{
+#ifdef GTENSOR_DEVICE_CUDA
+  auto result = cufftExecD2Z(handle, indata, outdata);
+  assert(result == CUFFT_SUCCESS);
+#elif defined(GTENSOR_DEVICE_HIP)
+  auto result = hipfftExecD2Z(handle, indata, outdata);
+  assert(result == rocfft_success);
+#endif
+}
