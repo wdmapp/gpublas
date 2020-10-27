d7d98abfeceac398f805c7da80f0ecafcce6c48d
Bryce Allen <bdallen@uchicago.edu>
2020-10-27 16:54:23 +0000

    more sycl fix attempts

--- src/gpufft.cxx
+++ src/gpufft.cxx
@@ -1,5 +1,10 @@
 #include <gtensor/gtensor.h>
 
+#ifdef GTENSOR_DEVICE_SYCL
+#include <iostream>
+#include <cstdlib>
+#endif
+
 #include "gpufft.h"
 
 void gpufft_plan_many(gpufft_handle_t* handle, int rank, int* n, int istride,
@@ -36,11 +41,6 @@ void gpufft_plan_many(gpufft_handle_t* handle, int rank, int* n, int istride,
   desc); assert(result == rocfft_success);
   */
 #elif defined(GTENSOR_DEVICE_SYCL)
-  std::vector<MKL_LONG> dims(rank);
-  for (int i = 0; i < rank; i++) {
-    dims[i] = n[i];
-  }
-  assert(dims.size() == rank);
   /*
   if (type == GPUFFT_R2C || type == GPUFFT_C2R) {
     auto h = new gpufft_single_handle_t(dims);
@@ -48,22 +48,52 @@ void gpufft_plan_many(gpufft_handle_t* handle, int rank, int* n, int istride,
     auto h = new gpufft_double_descriptor_t(dims);
   }
   */
-  gpufft_double_descriptor_t* h = new gpufft_double_descriptor_t(dims);
-  /*
-  h->set_value(oneapi::mkl::dft::config_param::INPUT_STRIDES, istride);
-  h->set_value(oneapi::mkl::dft::config_param::FWD_DISTANCE, 0);
-  h->set_value(oneapi::mkl::dft::config_param::OUTPUT_STRIDES, ostride);
-  h->set_value(oneapi::mkl::dft::config_param::BWD_DISTANCE, 0);
-  */
-  h->set_value(oneapi::mkl::dft::config_param::NUMBER_OF_TRANSFORMS,
-               batchSize);
-  h->set_value(oneapi::mkl::dft::config_param::PLACEMENT, DFTI_NOT_INPLACE);
-  h->set_value(oneapi::mkl::dft::config_param::REAL_STORAGE,
-               DFTI_REAL_REAL);
-  h->set_value(oneapi::mkl::dft::config_param::CONJUGATE_EVEN_STORAGE,
-               DFTI_COMPLEX_COMPLEX);
-  cl::sycl::queue& q = gt::backend::sycl::get_queue();
-  h->commit(q);
+  gpufft_double_descriptor_t* h;
+
+  try {
+    if (rank == 1) {
+      h = new gpufft_double_descriptor_t(n[0]);
+    } else {
+      std::vector<MKL_LONG> dims(rank);
+      for (int i = 0; i < rank; i++) {
+        dims[i] = n[i];
+      }
+      assert(dims.size() == rank);
+
+      h = new gpufft_double_descriptor_t(dims);
+    }
+  } catch(std::exception const& e) {
+    std::cerr << "Error creating dft descriptor:" << e.what() << std::endl;
+    abort();
+  }
+
+  try {
+    /*
+    h->set_value(oneapi::mkl::dft::config_param::INPUT_STRIDES, istride);
+    h->set_value(oneapi::mkl::dft::config_param::FWD_DISTANCE, 0);
+    h->set_value(oneapi::mkl::dft::config_param::OUTPUT_STRIDES, ostride);
+    h->set_value(oneapi::mkl::dft::config_param::BWD_DISTANCE, 0);
+    */
+    h->set_value(oneapi::mkl::dft::config_param::NUMBER_OF_TRANSFORMS,
+                 batchSize);
+    h->set_value(oneapi::mkl::dft::config_param::PLACEMENT, DFTI_NOT_INPLACE);
+    h->set_value(oneapi::mkl::dft::config_param::REAL_STORAGE,
+                 DFTI_REAL_REAL);
+    h->set_value(oneapi::mkl::dft::config_param::CONJUGATE_EVEN_STORAGE,
+                 DFTI_COMPLEX_COMPLEX);
+  } catch(std::exception const& e) {
+    std::cerr << "Error setting values on dft descriptor:" << e.what()
+              << std::endl;
+    abort();
+  }
+
+  try {
+    h->commit(gt::backend::sycl::get_queue());
+  } catch(std::exception const& e) {
+    std::cerr << "Error commiting dft descriptor:" << e.what() << std::endl;
+    abort();
+  }
+
   *handle = static_cast<void*>(h);
 #endif
 }
@@ -93,8 +123,8 @@ void gpufft_exec_z2d(gpufft_handle_t handle, gpufft_double_complex_t* indata,
   assert(result == rocfft_success);
 #elif defined(GTENSOR_DEVICE_SYCL)
   auto h = static_cast<gpufft_double_descriptor_t*>(handle);
-  //auto indata_double = reinterpret_cast<double*>(indata);
-  auto e = oneapi::mkl::dft::compute_backward(*h, indata, outdata);
+  auto indata_double = reinterpret_cast<double*>(indata);
+  auto e = oneapi::mkl::dft::compute_backward(*h, indata_double, outdata);
   e.wait();
 #endif
 }
@@ -110,8 +140,8 @@ void gpufft_exec_d2z(gpufft_handle_t handle, gpufft_double_real_t* indata,
   assert(result == rocfft_success);
 #elif defined(GTENSOR_DEVICE_SYCL)
   auto h = static_cast<gpufft_double_descriptor_t*>(handle);
-  //auto outdata_double = reinterpret_cast<double*>(outdata);
-  auto e = oneapi::mkl::dft::compute_forward(*h, indata, outdata);
+  auto outdata_double = reinterpret_cast<double*>(outdata);
+  auto e = oneapi::mkl::dft::compute_forward(*h, indata, outdata_double);
   e.wait();
 #endif
 }
